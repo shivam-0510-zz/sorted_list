81793b47cba3d024bf44eb837a03eb16fdde28b9
Shivam Gupta <shivam.gupta5102@gmail.com>
2021-06-29 12:13:18 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,5 @@
+l=[]
+for i in range(int(input("Enter number of elements for the list"))):
+    l+=[int(input("Enter element(an integer):"))]
+    print("The list is:\n")
+    print("The sorted list is:\n",sorted(l))
